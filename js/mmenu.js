06e0fcc10b567a5cd91bc762f5dbbde45ebd2ca5
Nazar <nazar.10.17k@gmail.com>
2020-10-22 14:52:48 +0300

    Mmenu: start

---
+++ js/mmenu.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	var menu_opened = false;
+	var menu = $('.header__nav');
+	
+	$(".j-menu-open").on("click", function (event) {
+		
+		if(!menu_opened) {
+			menu_opened = true;
+			menu.addClass('is-opened');
+			$("body").css("overflow","hidden");
+			$(".header__nav.is-opened .header__nav-container").css("top", $("header").height() + "px");
+			$(".header__container").css("background", "black");
+		}
+		else {
+			menu_opened = false;
+			menu.removeClass('is-opened');
+			$("body").css("overflow","auto");
+		}
+	});
+	
+	$(".j-menu-close").on("click", function (event) {
+			menu_opened = false;
+			menu.removeClass('is-opened');
+			$("body").css("overflow","auto");
+	});
+});
